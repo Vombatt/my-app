7c6ae9c93a98ed882c4e5ad00ca344050eeb278e
Alexey Goncharov <alexey.goncharov@vistar.su>
2019-05-06 14:07:21 +0300

    БЭМ

--- src/header/header.js
+++ src/header/header.js
@@ -5,10 +5,10 @@ import "./header.scss"
 export class Header extends Component {
   render() {
     return (
-      <div className="main-header">
-        <NavLink className="main-header-link" activeClassName="main-header-link-active" to="/" exact={true}>Стартовая страница</NavLink>
-        <NavLink className="main-header-link" activeClassName="main-header-link-active" to="/tictac">Крестики - нолики</NavLink>
-        <NavLink className="main-header-link" activeClassName="main-header-link-active" to="/tour">Тур героев</NavLink>
+      <div className="header">
+        <NavLink className="header__link" activeClassName="header__link_active" to="/" exact={true}>Стартовая страница</NavLink>
+        <NavLink className="header__link" activeClassName="header__link_active" to="/tictac">Крестики - нолики</NavLink>
+        <NavLink className="header__link" activeClassName="header__link_active" to="/tour">Тур героев</NavLink>
       </div>
     );
   }
